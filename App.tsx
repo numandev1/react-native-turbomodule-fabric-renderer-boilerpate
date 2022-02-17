00c7b283c60ef5b1b3d0c04a9bee36a801b54dd3
Muhammad Numan <muhammadnuman70@gmail.com>
2022-02-17 17:04:14 +0500

    chore: update rn version to 0.68.0-rc.1

--- App.tsx
+++ App.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
-import type {Node} from 'react';
-import {SafeAreaView, useColorScheme, StyleSheet, Alert} from 'react-native';
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  useColorScheme,
+  StyleSheet,
+  Dimensions,
+  Text,
+} from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import Button from 'react-native/Libraries/Components/Button';
 import MyApplicationLibrary from './Libraries/NativeMyApplicationLibrary';
 // import MyApplicationTurboCxxModule from './Libraries/NativeMyApplicationTurboCxxModule';
+const {width, height} = Dimensions.get('screen');
 const App: () => Node = () => {
   const isDarkMode = useColorScheme() === 'dark';
+  const [log, setLog] = useState('');
+  const makeLog = (obj: any) => {
+    let logStr = '';
+    for (let [key, value] of Object.entries(obj)) {
+      logStr += `\n\n${key} : ${value}`;
+    }
+    setLog(logStr);
+  };
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
@@ -16,12 +30,14 @@ const App: () => Node = () => {
   const testHandler = async () => {
     const response = await MyApplicationLibrary.getString('hello world ');
     console.log(response);
+    makeLog({response});
     // const response2 = await MyApplicationTurboCxxModule.getConstants();
     // console.log(response2);
   };
 
   return (
     <SafeAreaView style={[styles.container, backgroundStyle]}>
+      <Text style={styles.textInput}>{log}</Text>
       <Button
         onPress={testHandler}
         title="click here to test turbo module"
@@ -36,7 +52,15 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
+  },
+  textInput: {
+    height: height * 0.4,
+    width: width,
+    borderColor: 'gray',
+    borderWidth: 5,
+    fontSize: 20,
+    padding: 5,
+    marginBottom: 100,
   },
 });
